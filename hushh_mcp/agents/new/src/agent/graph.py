30fd7e108bcfd4a2eee461fd0051957d08a15918
ChaitIITB <23b2712@iitb.ac.in>
2025-07-30 20:57:52 +0530

    Done something

--- hushh_mcp/agents/new/src/agent/graph.py
+++ hushh_mcp/agents/new/src/agent/graph.py
@@ -1,7 +1,12 @@
 import getpass
 import os
+from typing import TypedDict
 from langchain_google_community import GoogleSearchAPIWrapper
 from langchain.tools import Tool
+from langgraph.prebuilt import create_react_agent
+from typing_extensions import Annotated, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
 
 def _set_if_undefined(var: str):
     if not os.environ.get(var):
@@ -17,9 +22,17 @@ google_search = Tool(
     func=search.run,
 )
 
+planning_tools = [
+    google_search,
+]
+
+tools = [
+    google_search,
+]
+
 supervisor_agent = create_react_agent(
     model="openai:gpt-4.1",
-    tools=[assign_to_research_agent, assign_to_math_agent],
+    tools=tools,
     prompt=(
         "You are a supervisor managing two agents:\n"
         "- a research agent. Assign research-related tasks to this agent\n"
@@ -28,4 +41,37 @@ supervisor_agent = create_react_agent(
         "Do not do any work yourself."
     ),
     name="supervisor",
+) 
+
+def create_coding_agents(name, llm: create_react_agent, tools: list[Tool]) -> create_react_agent:
+    """Create coding agents with the given LLM and tools."""
+    return create_react_agent(
+        name=name,
+        description="A coding agent that executes tasks assigned by the supervisor agent.",
+        model=llm,
+        tools=tools,
+        prompt=(
+            "You are a coding agent. Your task is to write code based on the instructions provided by the supervisor agent.\n"
+            "You will receive tasks from the supervisor agent and you should execute them sequentially."
+        ),
+        name="coding_agent",
+    )
+
+
+class InputState(TypedDict):
+    user_input: str
+
+class OutputState(TypedDict):
+    agent_results: list[dict]
+    summary: str
+
+class Int_State(TypedDict):
+    input: str
+    user_feedback: str
+    messages: Annotated[list[str], add_messages]
+
+graph_builder = StateGraph(
+
+    input_state=InputState,
+    output_state=OutputState
 )
